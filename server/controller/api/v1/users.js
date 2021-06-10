3192d719e701af355db70c881d2dad129bf621e4
nonuabi <nonuabi@gmail.com>
2021-06-10 23:06:09 +0530

    add update api

--- server/controller/api/v1/users.js
+++ server/controller/api/v1/users.js
@@ -1,5 +1,6 @@
 const User = require("../../../models/users");
 
+//sigup api
 module.exports.create = function (req, res) {
   // console.log(req.params);
 
@@ -35,6 +36,7 @@ module.exports.create = function (req, res) {
     });
 };
 
+//login api
 module.exports.login = function (req, res) {
   console.log(req.body.email);
   User.findOne({ email: req.body.email }, function (err, user) {
@@ -58,5 +60,42 @@ module.exports.login = function (req, res) {
   });
 };
 
+//update user details api
+module.exports.update = function (req, res) {
+  let body = req.body;
+  if (!body) {
+    return res.status(400).json({
+      success: false,
+      message: "You must provide a body for update",
+    });
+  }
+  console.log(req);
+  User.findOne({ _id: req.params.id }, function (err, user) {
+    if (err) {
+      return res.status(404).json({
+        success: false,
+        message: "user not found",
+      });
+    }
 
+    user.name = body.name;
+    user.email = body.email;
+    user.password = body.password;
 
+    user
+      .save()
+      .then(() => {
+        return res.status(200).json({
+          success: true,
+          message: "user details is updated",
+        });
+      })
+      .catch((err) => {
+        return res.status(404).json({
+          err,
+          success: false,
+          message: "user details not update",
+        });
+      });
+  });
+};
--- server/router/api/v1/users.js
+++ server/router/api/v1/users.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const userApi = require("../../../controller/api/v1/users");
 
 router.post("/create", userApi.create);
-router.post('/login', userApi.login);
+router.post("/login", userApi.login);
+router.post("/update", userApi.update);
 module.exports = router;
