bb4a408214b15440417ae014f5454510c877a015
nonuabi <nonuabi@gmail.com>
2021-06-14 21:48:24 +0530

    added style to the nav bar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,33 +1,46 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import { FiSearch } from "react-icons/fi";
+import "../css/Navbar.css";
 class Navbar extends Component {
   render() {
     return (
       <div className="navbar">
         <div className="logo-container">
-          <div className="logo-img">
-            <Link to="/">
-              <img
-                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS3yPH54tvGXpnR-YG4lLq6JJVQwnzQRBqO8Q&usqp=CAU"
-                alt="logo"
-              />
-            </Link>
-          </div>
+          <Link to="/">
+            <img
+              className="logo"
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS3yPH54tvGXpnR-YG4lLq6JJVQwnzQRBqO8Q&usqp=CAU"
+              alt="logo"
+            />
+          </Link>
+        </div>
+        <div>
           <div className="search-container">
-            <FiSearch />
+            <FiSearch className="search-icon" />
             <input placeholder="Search here.." />
           </div>
         </div>
-        <div className="nav-links">
-          <ul>
-            <li>
-              <Link to="/api/v1/users/login">Login</Link>
-            </li>
-            <li>
-              <Link to="/api/v1/users/create">SignUp</Link>
-            </li>
-          </ul>
+        <div className="right-nav">
+          <div className="user">
+            <img
+              src="https://image.flaticon.com/icons/svg/2154/2154651.svg"
+              alt="user-dp"
+              id="user-dp"
+            />
+            <span>John Doe</span>
+          </div>
+          <div className="nav-links">
+            <ul>
+              <li>
+                <Link to="/api/v1/users/login">Log In</Link>
+              </li>
+
+              <li>
+                <Link to="/api/v1/users/create">Sign Up</Link>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     );
