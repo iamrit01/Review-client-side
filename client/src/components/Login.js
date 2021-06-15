7066189ea4afa9f660e7363ff14ee7726aed9343
nonuabi <nonuabi@gmail.com>
2021-06-15 15:25:13 +0530

    add css file to login component

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,6 +1,6 @@
 import React, { Component, useState } from "react";
 import axios from "axios";
-
+import "../css/Login.css";
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -15,32 +15,34 @@ const Login = () => {
   };
   return (
     <div className="login">
-      <h1>Login Page</h1>
-      <div>
-        <label>
-          Email
-          <input
-            name="email"
-            type="email"
-            placeholder="Email"
-            value={email}
-            onChange={(event) => setEmail(event.target.value)}
-          />
-        </label>
+      <div className="login-container">
+        <h1>Login Page</h1>
+        <div className="input-container">
+          <label>
+            Email
+            <input
+              name="email"
+              type="email"
+              placeholder="Email"
+              value={email}
+              onChange={(event) => setEmail(event.target.value)}
+            />
+          </label>
+        </div>
+        <div className="input-container">
+          <label>
+            Password
+            <input
+              name="password"
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(event) => setPassword(event.target.value)}
+            />
+          </label>
+        </div>
+        <input type="submit" onClick={(e) => onSubmit(e)} value="submit" />
       </div>
-      <div>
-        <label>
-          Password
-          <input
-            name="password"
-            type="password"
-            placeholder="Password"
-            value={password}
-            onChange={(event) => setPassword(event.target.value)}
-          />
-        </label>
-      </div>
-      <input type="submit" onClick={(e) => onSubmit(e)} value="submit" />
     </div>
   );
 };
