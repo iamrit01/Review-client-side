37347d1468a7ffbec032dd4ff6c2935c54ad2b31
nonuabi <nonuabi@gmail.com>
2021-06-27 13:49:09 +0530

    added user id check function in create post api

--- server/controller/api/v1/post.js
+++ server/controller/api/v1/post.js
@@ -1,22 +1,34 @@
 const Post = require("../../../models/posts");
-const User = require('../../../models/users ');
+const User = require("../../../models/users");
 module.exports.create = async function (req, res) {
-  
   try {
-    User.findOne({_id : user})
-    let post = await Post.create({
-      Description: req.body.description,
-      user: req.body.id,
-    });
-    if (!post) {
-      return res.status(500).json({
-        message: "post is not create in db",
+    await User.findOne({ _id: req.body.id }, function (err, user) {
+      if (err) {
+        return res.status(500).json({
+          message: "error occure while finding the user in the db",
+          error: err,
+        });
+      }
+      if (!user) {
+        return res.status(401).json({
+          message: "user is not avable in db",
+        });
+      }
+      let post = Post.create({
+        Description: req.body.description,
+        user: req.body.id,
+      }).then((post) => {
+        if (!post) {
+          return res.status(500).json({
+            message: "post is not create in db",
+          });
+        }
+        console.log("post details ", post);
+        return res.status(200).json({
+          message: "post is created",
+          createdPost: post,
+        });
       });
-    }
-    console.log("post details ", post);
-    return res.status(200).json({
-      message: "post is created",
-      createdPost: post,
     });
   } catch (err) {
     return res.status(500).json({
