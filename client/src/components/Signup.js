bda30fbf88ac6fdcb5c921bc63b130410d462c04
nonuabi <nonuabi@gmail.com>
2021-07-24 01:03:03 +0530

    change class componet to hooks and replace axios to fetch api

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,111 +1,146 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
+import { useHistory } from "react-router";
 import axios from "axios";
 import "../css/Signup.css";
-class Signup extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      user: {
-        name: "",
-        email: "",
-        password: "",
-        confirmPassword: "",
-      },
-    };
-  }
-  onNameChange = (e) => {
-    this.setState({
-      user: {
-        ...this.state.user,
-        name: e.target.value,
-      },
-    });
+const Signup = () => {
+  const history = useHistory();
+  const [user, setUser] = useState({
+    name: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+  let name, value;
+  const handleInputChanges = (e) => {
+    name = e.target.name;
+    value = e.target.value;
+    setUser({ ...user, [name]: value });
   };
-  onEmailChange = (e) => {
-    this.setState({
-      user: {
-        ...this.state.user,
-        email: e.target.value,
+  // onNameChange = (e) => {
+  //   this.setState({
+  //     user: {
+  //       ...this.state.user,
+  //       name: e.target.value,
+  //     },
+  //   });
+  // };
+  // onEmailChange = (e) => {
+  //   this.setState({
+  //     user: {
+  //       ...this.state.user,
+  //       email: e.target.value,
+  //     },
+  //   });
+  // };
+  // onPasswordChange = (e) => {
+  //   this.setState({
+  //     user: {
+  //       ...this.state.user,
+  //       password: e.target.value,
+  //     },
+  //   });
+  // };
+  // onconfirmPasswordChange = (e) => {
+  //   this.setState({
+  //     user: {
+  //       ...this.state.user,
+  //       confirmPassword: e.target.value,
+  //     },
+  //   });
+  // };
+
+  const signUpUser = async (event) => {
+    event.preventDefault();
+    const { name, email, password, confirmPassword } = user;
+    const response = await fetch("/api/v1/users/signup", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
       },
+      body: JSON.stringify({
+        name,
+        email,
+        password,
+        confirmPassword,
+      }),
     });
-  };
-  onPasswordChange = (e) => {
-    this.setState({
-      user: {
-        ...this.state.user,
-        password: e.target.value,
-      },
-    });
-  };
-  onconfirmPasswordChange = (e) => {
-    this.setState({
-      user: {
-        ...this.state.user,
-        confirmPassword: e.target.value,
-      },
-    });
+    const data = await response.json();
+    if (!data || response.status === 422) {
+      console.log("Invalid Credentials");
+      window.alert("Invalid Credentials");
+    } else if (response.status === 500) {
+      console.log("Server Error! 500");
+      window.alert("Server Error! 500");
+    } else {
+      history.push("/api/v1/users/login");
+      window.alert("Registration  Successfully :)");
+    }
+    // console.log("this.state.user ", this.state.user);
+    // await axios
+    //   .post("/api/v1/users/signup", this.state.user)
+    //   .then((response) => {
+    //     console.log("response ", response);
+    //     // localStorage.item('token', response.to)
+    //     this.props.history.push("/api/v1/users/login");
+    //   })
+    //   .catch((e) => {
+    //     console.log(e);
+    //   });
   };
 
-  signUPData = async (event) => {
-    event.preventDefault();
-    console.log("this.state.user ", this.state.user);
-    await axios
-      .post("/api/v1/users/signup", this.state.user)
-      .then((response) => {
-        console.log("response ", response);
-        // localStorage.item('token', response.to)
-        this.props.history.push("/api/v1/users/login");
-      })
-      .catch((e) => {
-        console.log(e);
-      });
-  };
-  render() {
-    return (
-      <form className="login-form">
-        <span className="login-signup-header">Sign Up</span>
-        <div className="field">
-          <input
-            type="text"
-            value={this.state.user.name}
-            placeholder="Enter Name"
-            required
-            onChange={this.onNameChange}
-          />
-        </div>
-        <div className="field">
-          <input
-            type="email"
-            value={this.state.user.email}
-            placeholder="Enter Email"
-            required
-            onChange={this.onEmailChange}
-          />
-        </div>
-        <div className="field">
-          <input
-            type="password"
-            value={this.state.user.password}
-            placeholder="Enter Password"
-            required
-            onChange={this.onPasswordChange}
-          />
-        </div>
-        <div className="field">
-          <input
-            type="password"
-            value={this.state.user.confirmPassword}
-            placeholder="Re-Enter Password"
-            required
-            onChange={this.onconfirmPasswordChange}
-          />
-        </div>
-        <div className="field">
-          <button onClick={this.signUPData}>Sign Up</button>
-        </div>
-      </form>
-    );
-  }
-}
+  return (
+    <form className="login-form" method="POST">
+      <span className="login-signup-header">Sign Up</span>
+      <div className="field">
+        <input
+          placeholder="Enter Name"
+          name="name"
+          type="text"
+          value={user.name}
+          onChange={handleInputChanges}
+          required
+        />
+      </div>
+      <div className="field">
+        <input
+          placeholder="Enter Email"
+          name="email"
+          type="email"
+          value={user.email}
+          onChange={handleInputChanges}
+          required
+        />
+      </div>
+      <div className="field">
+        <input
+          placeholder="Enter Password"
+          name="password"
+          type="password"
+          value={user.password}
+          onChange={handleInputChanges}
+          required
+        />
+      </div>
+      <div className="field">
+        <input
+          placeholder="Re-Enter Password"
+          name="confirmPassword"
+          type="password"
+          value={user.confirmPassword}
+          onChange={handleInputChanges}
+          required
+        />
+      </div>
+      <div className="field">
+        <input
+          type="submit"
+          name="signUp"
+          value="signUp"
+          onClick={signUpUser}
+        />
+      </div>
+    </form>
+  );
+};
 
 export default Signup;
