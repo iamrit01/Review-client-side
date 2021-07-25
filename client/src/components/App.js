276028cca5864666037b5d274bb1d91408fb0268
nonuabi <nonuabi@gmail.com>
2021-07-25 14:53:37 +0530

    remove commented part

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -13,36 +13,14 @@ import { createContext, useReducer, useState } from "react";
 import { Route, Switch } from "react-router-dom";
 import { initialState, reducer } from "../reducer/UseReducer";
 export const UserContext = createContext();
-const Routing = (handleUserName) => {
-  console.log("app props", handleUserName);
-  return (
-    <Switch>
-      <Route exact path="/" component={Timeline} />
-      <Route path="/login" component={Login} />
-      <Route path="/signUp" component={Signup} />
-      <Route exact path="/profile" component={Profile} />
-      <Route path="/about" component={About} />
-      <Route
-        path="/logout"
-        render={() => <Login handleName={handleUserName()} />}
-      />
-      <Route exact component={NotFound} />
-    </Switch>
-  );
-};
+
 const App = () => {
   const [state, dispatch] = useReducer(reducer, initialState);
   const [name, setName] = useState("");
-  const handleNameChange = (name) => {
-    setName({
-      name,
-    });
-  };
   return (
     <>
       <UserContext.Provider value={{ state, dispatch }}>
         <Navbar name={name} />
-        {/* <Routing handleUserName={(name) => setName(name)} /> */}
         <Switch>
           <Route exact path="/" component={Timeline} />
           <Route
