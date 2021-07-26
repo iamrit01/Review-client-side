0bb680bc47905a0e08e6ece93b03b8642b7c1216
nonuabi <nonuabi@gmail.com>
2021-07-26 16:16:57 +0530

    profile is created

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,8 +1,53 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import "../css/Profile.css";
 const Profile = () => {
+  const [user, setUser] = useState({
+    name: "",
+    email: "",
+    profileImage: "",
+  });
+  useEffect(
+    () => {
+      displayProfile();
+    },
+    { ...user }
+  );
+
+  const displayProfile = async () => {
+    const res = await fetch("/api/v1/profile/profile", {
+      method: "GET",
+      headers: {
+        Accept: "appllication/json",
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+    });
+    const data = await res.json();
+    console.log("fron end side profile data", data);
+
+    setUser({
+      ...user,
+      name: data.name,
+      email: data.email,
+      profileImage: data.profileImage,
+    });
+  };
+  console.log("profile state ", user);
   return (
-    <div>
-      <h1>User Profile</h1>
+    <div className="profile_main_container">
+      <div className="image_container">
+        <img src={`${user.profileImage}`} alt="..." />
+      </div>
+      <div className="info_container">
+        <div className="info_item">
+          <h1>Name :&nbsp;</h1>
+          <h1>{user.name}</h1>
+        </div>
+        <div className="info_item">
+          <p>Email :&nbsp;</p>
+          <p>{user.email}</p>
+        </div>
+      </div>
     </div>
   );
 };
--- server/controller/api/v1/profiles.js
+++ server/controller/api/v1/profiles.js
@@ -1,4 +1,5 @@
 const jwt = require("jsonwebtoken");
+const User = require("../../../models/users");
 const dotenv = require("dotenv").config();
 module.exports.timeLine = function (req, res) {
   console.log("hello timeline page ");
@@ -25,5 +26,10 @@ module.exports.timeLine = function (req, res) {
 // });
 
 module.exports.personal = function (req, res) {
-  console.log("personal", req);
+  console.log("root.user ", req.rootUser);
+  return res.status(200).json({
+    name: req.rootUser.name,
+    email: req.rootUser.email,
+    profileImage: req.rootUser.profileImage,
+  });
 };
