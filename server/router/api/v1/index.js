d741a3644697fcc9dc6040a4baf133146e452081
nonuabi <nonuabi@gmail.com>
2021-09-04 12:54:35 +0530

    client Side

--- server/router/api/v1/index.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.use("/users", require("./users"));
-router.use("/profile", require("./Profiles"));
-router.use("/post", require("./post"));
-router.use("/comment", require("./comment"));
-module.exports = router;
